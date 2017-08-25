724bbd132c73675f9017c21705962c12af04570a
JavaRubyNode <viniciusjavaruby@gmail.com>
2017-08-24 22:50:03 -0300

    comiti inicial

---
+++ TrabalhoPosSpring/src/main/java/com/vinicius/pos/TrabalhoPosSpringApplication.java
@@ -0,0 +1,12 @@
+package com.vinicius.pos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabalhoPosSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabalhoPosSpringApplication.class, args);
+	}
+}
