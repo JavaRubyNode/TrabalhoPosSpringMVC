2b207d984d61d60c58da7a2e065cefbd650724cb
JavaRubyNode <viniciusjavaruby@gmail.com>
2017-08-26 00:42:56 -0300

    removido a interface generica pois nao aceita

--- TrabalhoPosSpring/src/main/java/com/vinicius/pos/repository/IRepositoryGenericDAO.java
+++
@@ -1,7 +0,0 @@
-package com.vinicius.pos.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IRepositoryGenericDAO<T> extends JpaRepository<T, Long>{
-
-}
